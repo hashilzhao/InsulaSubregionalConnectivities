82dbf3ac6262ac2533fcd78e4e78fb417ad1f33c
Haichao Zhao <45781030+hashilzhao@users.noreply.github.com>
2021-11-03 01:22:46 +0800

    Create s01_preprocessing.sh

---
+++ s01_preprocessing.sh
@@ -0,0 +1,51 @@
+## pwd = /expdata2/insula.validation
+
+logdate=`date +%Y%m%d_%T`
+basedir=/expdata2/insula.validation
+datadir=/expdata2/insula.validation/data
+
+mkdir -p ${basedir}/logs
+
+b1s=$(ls ${datadir})
+pipe (){
+local sta=$1
+local num=$2
+echo "process $sta : $((sta + num))"
+
+for sub in  $(echo $b1s | cut -f$sta-$((sta+num)) -d " ")
+do
+  echo $sub
+  sed -e "s/sub25632/${sub}/g" ${basedir}/design/prepro.fsf > ${datadir}/${sub}/analysis/prepro.fsf
+  feat ${datadir}/${sub}/analysis/prepro.fsf
+  fast -t 1 -n 3 -H 0.1 -I 4 -l 20.0 -o ${datadir}/${sub}/data/mprage_brain ${datadir}/${sub}/data/mprage_brain
+
+  echo 'sed' -e "s/sub25632/${sub}/g" ${basedir}/design/prepro.fsf '>' ${datadir}/${sub}/analysis/prepro.fsf >> ${basedir}/logs/pro_log_${sub}_s1.txt
+  echo feat ${datadir}/${sub}/analysis/prepro.fsf >> $basedir/logs/pro_log_${sub}_s1.txt
+  echo fast -t 1 -n 3 -H 0.1 -I 4 -l 20.0 -o ${datadir}/${sub}/data/mprage_brain ${datadir}/${sub}/data/mprage_brain >> ${basedir}/logs/pro_log_${sub}_s1.txt
+done
+}
+pipe 1 9 &
+pipe 11 9 &
+pipe 21 9 &
+pipe 31 9 &
+pipe 41 9 &
+pipe 51 9 &
+pipe 61 9 &
+pipe 71 9 &
+pipe 81 9 &
+pipe 91 9 &
+pipe 101 9 &
+pipe 111 9 &
+pipe 121 9 &
+pipe 131 9 &
+pipe 141 9 &
+pipe 151 9 &
+pipe 161 9 &
+pipe 171 9 &
+pipe 181 9 &
+pipe 191 9 &
+pipe 201 9 &
+pipe 211 9 &
+pipe 221 6 &
+wait
+echo -e "time-consuming: $SECONDS seconds"
