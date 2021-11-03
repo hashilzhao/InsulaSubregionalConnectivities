75e1174eae156d50e0ffe48550769d1b482f7a14
Haichao Zhao <45781030+hashilzhao@users.noreply.github.com>
2021-11-03 16:16:12 +0800

    Update s10_group_analysis.sh

--- s10_group_analysis.sh
+++ s10_group_analysis.sh
@@ -2,34 +2,34 @@
 logdate=`date +%Y%m%d_%T`
 basedir=/expdata2/insula.validation
 datadir=/expdata2/insula.validation/data
+designdir=/expdata2/insula.validation/design
 
 mkdir -p ${basedir}/logs
 
-feat ${basedir}/design/glDAI.fsf &
-feat ${basedir}/design/glVAI.fsf &
-feat ${basedir}/design/glPI.fsf &
-feat ${basedir}/design/grDAI.fsf &
-feat ${basedir}/design/grVAI.fsf &
-feat ${basedir}/design/grPI.fsf &
-feat ${basedir}/design/glDAI_lPI.fsf &
-feat ${basedir}/design/glDAI_lVAI.fsf &
-feat ${basedir}/design/glVAI_lPI.fsf &
-feat ${basedir}/design/grDAI_rPI.fsf &
-feat ${basedir}/design/grDAI_rVAI.fsf &
-feat ${basedir}/design/grVAI_rPI.fsf 
+feat ${designdir}/glDAI.fsf &
+feat ${designdir}/glVAI.fsf &
+feat ${designdir}/glPI.fsf &
+feat ${designdir}/grDAI.fsf &
+feat ${designdir}/grVAI.fsf &
+feat ${designdir}/grPI.fsf &
+feat ${designdir}/glDAI_lPI.fsf &
+feat ${designdir}/glDAI_lVAI.fsf &
+feat ${designdir}/glVAI_lPI.fsf &
+feat ${designdir}/grDAI_rPI.fsf &
+feat ${designdir}/grDAI_rVAI.fsf &
+feat ${designdir}/grVAI_rPI.fsf 
 
-echo feat ${basedir}/design/glDAI.fsf '&' >> ${basedir}/logs/s10.txt
-echo feat ${basedir}/design/glVAI.fsf '&' >> ${basedir}/logs/s10.txt
-echo feat ${basedir}/design/glPI.fsf '&' >> ${basedir}/logs/s10.txt
-echo feat ${basedir}/design/grDAI.fsf '&' >> ${basedir}/logs/s10.txt
-echo feat ${basedir}/design/grVAI.fsf '&' >> ${basedir}/logs/s10.txt
-echo feat ${basedir}/design/grPI.fsf '&' >> ${basedir}/logs/s10.txt
-echo feat ${basedir}/design/glDAI_lPI.fsf '&' >> ${basedir}/logs/s10.txt
-echo feat ${basedir}/design/glDAI_lVAI.fsf '&' >> ${basedir}/logs/s10.txt
-echo feat ${basedir}/design/glVAI_lPI.fsf '&' >> ${basedir}/logs/s10.txt
-echo feat ${basedir}/design/grDAI_rPI.fsf '&' >> ${basedir}/logs/s10.txt
-echo feat ${basedir}/design/grDAI_rVAI.fsf '&' >> ${basedir}/logs/s10.txt
-echo feat ${basedir}/design/grVAI_rPI.fsf >> ${basedir}/logs/s10.txt
-
+echo feat ${designdir}/glDAI.fsf '&' >> ${basedir}/logs/s10.txt
+echo feat ${designdir}/glVAI.fsf '&' >> ${basedir}/logs/s10.txt
+echo feat ${designdir}/glPI.fsf '&' >> ${basedir}/logs/s10.txt
+echo feat ${designdir}/grDAI.fsf '&' >> ${basedir}/logs/s10.txt
+echo feat ${designdir}/grVAI.fsf '&' >> ${basedir}/logs/s10.txt
+echo feat ${designdir}/grPI.fsf '&' >> ${basedir}/logs/s10.txt
+echo feat ${designdir}/glDAI_lPI.fsf '&' >> ${basedir}/logs/s10.txt
+echo feat ${designdir}/glDAI_lVAI.fsf '&' >> ${basedir}/logs/s10.txt
+echo feat ${designdir}/glVAI_lPI.fsf '&' >> ${basedir}/logs/s10.txt
+echo feat ${designdir}/grDAI_rPI.fsf '&' >> ${basedir}/logs/s10.txt
+echo feat ${designdir}/grDAI_rVAI.fsf '&' >> ${basedir}/logs/s10.txt
+echo feat ${designdir}/grVAI_rPI.fsf >> ${basedir}/logs/s10.txt
 
 echo -e "time-consuming: $SECONDS seconds" 
